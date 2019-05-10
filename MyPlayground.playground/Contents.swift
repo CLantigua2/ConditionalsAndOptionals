3c9ffef5bacdb8efb031b6babc60ca0a0fe02d9d
Clantigua2 <clantigua954@gmail.com>
2019-05-10 11:18:26 -0400

    completed first true false task

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+print(15 == 15) // true
+
+print(15 != 15) // false
+
+print( 42 > 90) // false
+
+print( 84 != 84) // false
+
+print(4 <= 4) // true
+
+print(4 >= 5) // false
+
+print((42 > 88) && (42 < 88)) // false
+
+print(!true) // false
