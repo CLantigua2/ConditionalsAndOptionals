1b0caed73652a0ad45ab74f669f36be17c3b286a
Clantigua2 <clantigua954@gmail.com>
2019-05-10 13:02:32 -0400

    completed the 4th assignment with a dict

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -44,3 +44,42 @@ if (steps < stepGoal / 2 && steps > stepGoal / 10) {
 } else {
     print("Yay you did it")
 }
+
+/*Imagine you're going to dinner with friends and are struggling to decide where to go. Two of you have very strong opinions and have clearly laid out your requirements for dinner as follows:
+ 
+ You want to eat somewhere that has either fish or pizza
+ Your friend wants to eat somewhere with vegan options.
+ 
+ Another friend brings up a restaurant she thinks will fit both of your criteria. This restaurant's attributes are represented by a few constants below. Write an if-else statement that will print "Let's go!" if the restaurant's attributes match the group's dietary requirements, and otherwise will print "Sorry, we'll have to think of somewhere else." */
+
+let hasFish = true
+let hasPizza = false
+let hasVegan = true
+
+var restourantA: [String: Bool] = ["hasFish": false, "hasPizza": true, "hasVegan": true]
+
+var restourantB: [String: Bool] = ["hasFish": true, "hasPizza": false, "hasVegan": true
+]
+var restourantC: [String: Bool] = ["hasFish": false, "hasPizza": false, "hasVegan": true
+]
+
+print(restourantA["hasFish"]!)
+
+if (restourantA["hasFish"]! == hasFish && restourantA["hasPizza"]! == hasPizza && restourantA["hasVegan"]! == hasVegan) {
+    print("we're going to restaurantA")
+} else if (restourantB["hasFish"]! == hasFish && restourantB["hasPizza"]! == hasPizza && restourantB["hasVegan"]! == hasVegan) {
+    print("we're going to restaurantB")
+} else if (restourantC["hasFish"]! == hasFish && restourantC["hasPizza"]! == hasPizza && restourantA["hasVegan"]! == hasVegan) {
+    print("we're going to restaurantC")
+}
+
+/* Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and sunny out. Create a constant isNiceWeather that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice. */
+
+let temp = 82
+let isRaining = true
+let isSunny = true
+
+if (temp >= 82 && isSunny) {
+    print("It's nice enough to go outside")
+}
+
